afb840b651fbc458be700a04bd1d17f858ffa1b3
Reuven V. Gonzales <reuven@tobetter.us>
2014-10-28 02:14:28 -0700

    Pushing fix for workaround

--- sudolikeaboss-workaround.rb
+++ sudolikeaboss-workaround.rb
@@ -2,10 +2,10 @@ require 'formula'
 
 class SudolikeabossWorkaround < Formula
   homepage 'https://github.com/ravenac95/sudolikeaboss-workaround'
-  version '0.1.0'
+  version '0.1.1'
 
-  url 'http://dl.bintray.com/ravenac95/sudolikeaboss/sudolikeaboss-workaround_0.1.0_darwin_amd64.zip'
-  sha256 'b3430d2d08f21411d910d5a6779ac38596eb1a0990be6f7e2e41b36a053b4eb0'
+  url 'http://dl.bintray.com/ravenac95/sudolikeaboss/sudolikeaboss-workaround_0.1.1_darwin_amd64.zip'
+  sha256 'e5dc681c4debcc8b86cbed5093fc0a405ce113579b95e31b4fadefc8817d0d3a'
 
   depends_on :arch => :intel
 
