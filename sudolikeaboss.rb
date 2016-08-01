aaa20fdf121d4057a91a3de08324511c8cd00d05
Reuven V. Gonzales <reuven.v.gonzales@gmail.com>
2016-08-01 13:31:05 -0700

    Fix installation

--- sudolikeaboss.rb
+++ sudolikeaboss.rb
@@ -4,7 +4,7 @@ class Sudolikeaboss < Formula
   homepage 'https://github.com/ravenac95/sudolikeaboss'
   version '0.2.1'
 
-  if Hardware.is_64_bit?
+  if Hardware::CPU.is_64_bit?
     url 'http://dl.bintray.com/ravenac95/sudolikeaboss/sudolikeaboss_0.2.1_darwin_amd64.zip'
     sha256 '23c50e9b6190930f1760122a396a1d6778fbd83266eaf3066623954ec7f0aedd'
   end
